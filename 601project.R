5b094171b09d79fffb931b0312bfc3a56778c3f3
Laurakn <laura.niss@gmail.com>
2017-04-03 18:17:31 -0400

    Inital commit

---
+++ 601project.R
@@ -0,0 +1,5 @@
+df.all <- read.table("/Users/lauraniss/Documents/Data/601/Project/gvdb-aggregated-db/Events.tsv", 
+                 sep="\t", header=TRUE, fill=TRUE)
+
+df.fatal <- read.csv("/Users/lauraniss/Documents/Data/601/Project/data-police-shootings-master
+                       /fatal-police-shootings-data.csv", sep="\t", header=TRUE, fill=TRUE)
