ca57fc97dd520bbae0861ddb604fa3619af6b3f7
Laurakn <laura.niss@gmail.com>
2017-04-03 18:20:52 -0400

    Initial commit

--- 601project.R
+++ 601project.R
@@ -1,5 +1,5 @@
-df.all <- read.table("/Users/lauraniss/Documents/Data/601/Project/gvdb-aggregated-db/Events.tsv", 
+df.all <- read.table("~/Documents/Data/601/Project/gvdb-aggregated-db/Events.tsv", 
                  sep="\t", header=TRUE, fill=TRUE)
 
-df.fatal <- read.csv("/Users/lauraniss/Documents/Data/601/Project/data-police-shootings-master
-                       /fatal-police-shootings-data.csv", sep="\t", header=TRUE, fill=TRUE)
+df.fatal <- read.csv("~/Documents/Data/601/Project/data-police-shootings-master/fatal-police-shootings-data.csv")
+
